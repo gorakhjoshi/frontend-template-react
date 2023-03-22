84bf933244dd6c911c9113b1b508eb4c4811c5bb
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-03-22 23:31:20 +0545

    init react-ts-tailwind template

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "airbnb-typescript", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/require-default-props": ["error", { functions: "defaultArguments" }],
+    "react/react-in-jsx-scope": 0,
+    "import/prefer-default-export": 0,
+  },
+};
